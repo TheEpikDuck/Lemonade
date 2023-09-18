47384935e2e22cf43d9c7130469ed0a2444d145d
TheEpikDuck <zhujul24@7hills.org>
2023-09-18 13:06:05 -0400

    added a comment

--- Lemonade.java
+++ Lemonade.java
@@ -2,6 +2,8 @@ public class Lemonade
 {
     public class Store
     {
-        System.out.print(" Welcome. How many cups would you like to buy? 10 for ")
+        System.out.print(" Welcome. How many cups would you like to buy? 10 for $0.50, 25 for $1.50, and 50 for $")
     }
 }
+
+//Julia
